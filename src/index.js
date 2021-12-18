d34cfcc6cc139b9adecff965c643fae899767ae4
SURAJ KUMAR <sksurajkumar7090@gmail.com>
2021-12-18 12:07:34 +0530

    update  index

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const bodyparser = require('body-parser')
 
 
 const registerStudentController = require('./controllers/registerStudent.controller')
-const loginStudentController= require('./controllers/loginStudent.controller');
+const loginStudentController = require('./controllers/loginStudent.controller');
 const studentprofilesController = require('./controllers/studentprofile.controller.js')
+const courseController = require("./controllers/course.controller");
+const cartController = require("./controllers/cart.controller");
 const app = express();
 
 app.use(express.json());
@@ -16,9 +18,12 @@ app.use(express.static("public")) // for adding js and css file
 
 //set templating Enginge
 app.set("view engine", "ejs");
-app.use(bodyparser.urlencoded({extended : true}))
+app.use(bodyparser.urlencoded({ extended: true }))
 
 app.use('/register', registerStudentController);
 app.use("/login", loginStudentController);
-app.use("/profile",studentprofilesController)
+app.use("/profile", studentprofilesController);
+app.use("/cart", cartController);
+app.use("/courses", courseController);
+
 module.exports = app;
